b3f6cacec3b051d2d9b42403b67fd5d1cc96c1cd
eyupcan0 <eyupcanekizz@gmail.com>
2024-08-07 14:04:39 +0300

    profile sayfası güncellemeleri

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      // Dosya işlemlerini burada gerçekleştirebilirsiniz (örneğin, sunucuya yüklemek için)
+      console.log('Selected file:', file);
+    }
+  }
 }
