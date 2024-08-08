ef2b372a7ff092f350fc44cb950c7142ec8483d7
Alperenakkal <alperenakkal06@gmail.com>
2024-08-08 10:26:24 +0300

    localstoragetoken

--- src/app/components/login/auth.service.ts
+++ src/app/components/login/auth.service.ts
@@ -4,6 +4,8 @@ import { Observable, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { LoginModel } from './loginModel';  // Ensure the path is correct
 import { userModel } from './userModel';
+import { CookieService } from 'ngx-cookie-service';
+
 
 interface LoginResponse {
   accessTokenExpireDate: string;
@@ -20,7 +22,7 @@ export class AuthService {
   private tokenUrl = 'https://booklibaryapi.azurewebsites.net/api/User/redis%20get';
   private userUrl = 'https://booklibaryapi.azurewebsites.net/api/BorrowBook/user/';
   
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookieService:CookieService) {}
 
   public login(username: string, password: string): Observable<LoginResponse> {
     const loginData: LoginModel = { username, password };
@@ -29,12 +31,17 @@ export class AuthService {
     });
   }
 
-  public getToken(): Observable<string> {
-    return this.http.get(this.tokenUrl, {
-      withCredentials: true,
-      responseType: 'text' // Process the response as text
-    });
-  }
+  public getTokenLocal(): Observable<string> {
+    const token = localStorage.getItem("AuthToken") || '';
+    return of(token);
+}
+public getToken(): Observable<string> {
+  return this.http.get(this.tokenUrl, {
+    withCredentials: true,
+    responseType: 'text' // Yanıtı text olarak işle
+  });
+}
+
 
   private extractUserIdFromToken(token: string): string {
     const payload = token.split('.')[1];
@@ -44,7 +51,7 @@ export class AuthService {
   }
 
   public getById(): Observable<userModel> {
-    return this.getToken().pipe(
+    return this.getTokenLocal().pipe(
       map(token => this.extractUserIdFromToken(token)),
       switchMap(userId => this.http.get<userModel>(`${this.userUrl}${userId}`, {
         withCredentials: true,
