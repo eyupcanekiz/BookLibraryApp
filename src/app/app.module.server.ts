5b632e43facfd3bcb36a3e588868d295ea47117d
esmanurgokkaya <esmanurgkky10@gmail.com>
2024-08-08 15:49:36 +0300

    spinner

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,6 +3,7 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   imports: [
     AppModule,
