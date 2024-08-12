1a6ca8157a8fc23e6cb4d67601867158971a79bf
eyupcan0 <eyupcanekizz@gmail.com>
2024-08-12 11:30:49 +0300

    verification email

--- src/app/verification-enter/verification-enter.component.ts
+++ src/app/verification-enter/verification-enter.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { VerificationCodeService } from './verification-code.service'; // Servisi import edin
 
 @Component({
   selector: 'app-verification-enter',
@@ -8,13 +9,17 @@ import { Router } from '@angular/router';
 })
 export class VerificationEnterComponent {
   verificationCode: string = '';  // Kullanıcının girdiği kod
-  correctCode: string = '123456'; // Bu, backend'den gelen doğru kod olmalıdır
   errorMessage: string = '';      // Hataları göstermek için
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private verificationCodeService: VerificationCodeService // Servisi inject edin
+  ) {}
 
   onSubmit() {
-    if (this.verificationCode === this.correctCode) {
+    const correctCode = this.verificationCodeService.getVerificationCode(); // Doğru kodu servisten alın
+
+    if (this.verificationCode === correctCode) {
       // Kod doğru ise
       console.log('Girdiğiniz kod doğru!');
       alert('Girdiğiniz kod doğru!');
