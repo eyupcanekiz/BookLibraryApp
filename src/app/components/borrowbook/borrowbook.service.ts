b29a22552972fbcc1a37594ed5df66ff6318b802
Haluk Aytış <halukaytis@hotmail.com>
2024-08-13 15:18:11 +0300

    removeborrowedbook taslak

--- src/app/components/borrowbook/borrowbook.service.ts
+++ src/app/components/borrowbook/borrowbook.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book } from '../book/book.model';
 import { Observable } from 'rxjs';
@@ -7,13 +7,25 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class BorrowbookService {
-private apiUrl = 'https://booklibaryapi.azurewebsites.net/api/BorrowBook';
+  private apiUrl = 'https://booklibaryapi.azurewebsites.net/api/BorrowBook';
+
   constructor(private http: HttpClient) { }
 
   getBorrowedBooks(userId: string): Observable<Book[]> {
     return this.http.get<Book[]>(`${this.apiUrl}/GetBorrowBooks?id=${userId}`);
-}
-addBorrowedBook(bookId: string, userId: string): Observable<string> {
-  return this.http.post<string>(`${this.apiUrl}/AddBorrow?id=${userId}`, { Id: bookId });
-}
+  }
+
+  addBorrowedBook(bookId: string, userId: string): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/AddBorrow?id=${userId}`, { Id: bookId });
+  }
+
+  removeBorrowedBook(userId: string, bookId: string): Observable<any> {
+    const url = `${this.apiUrl}/RemoveBorrowed/${userId}`;
+    return this.http.delete(url, { 
+      body: { 
+        id : bookId  // JSON verisinin doğru formatta olduğundan emin olun
+      } 
+    });
+  }
+  
 }
