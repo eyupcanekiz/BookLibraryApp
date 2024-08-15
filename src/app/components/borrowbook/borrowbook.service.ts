17a51f8c3dc8531adec6b18faf40f391f3812ffb
Haluk Aytış <halukaytis@hotmail.com>
2024-08-15 11:59:29 +0300

    .

--- src/app/components/borrowbook/borrowbook.service.ts
+++ src/app/components/borrowbook/borrowbook.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 export interface BorrowBookByNameDto {
-  bookNameDto: string;
+    bookNameDto: string;
 }
 
 @Injectable({
