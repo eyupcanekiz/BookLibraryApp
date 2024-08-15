28d87341161929f597c91a7abe4d32d4d358a2bb
Alperenakkal <alperenakkal06@gmail.com>
2024-08-15 10:57:24 +0300

    allbookshow

--- src/app/all-book-show/all-book-show.component.ts
+++ src/app/all-book-show/all-book-show.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BookService, Book } from '../components/book/book.service';
 import { response } from 'express';
 import { error } from 'console';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AllBookShowService, AllShowBookDto } from './all-book-show.service';
 
 @Component({
   selector: 'app-all-book-show',
@@ -18,18 +19,27 @@ export class AllBookShowComponent implements OnInit {
   publisher:string =""
   bookId: string = '';
   author:string ="";
+  userName:string="Alperen";
+  bookNameDto!:AllShowBookDto;
   
   available : boolean = false;
   book: Book | null = null;
   constructor(
     private route: ActivatedRoute,
     private bookService: BookService,
-    private snackBar:MatSnackBar
+    private snackBar:MatSnackBar,
+    private allBookShowService:AllBookShowService,
+    private router : Router,
+  
+
   ) {}
 
   ngOnInit(){
+   
     const name = this.route.snapshot.paramMap.get('name');
     this.onGetByName(name!);
+    
+  
    }
   onGetByName(name:string){
     this.bookService.getBookByName(name).subscribe(
@@ -39,6 +49,7 @@ export class AllBookShowComponent implements OnInit {
           this.publisher =response.publisher
           this.author =response.author
           this.available=response.isAvailable
+      
         },
         error:(error) =>{
           this.snackBar.open('Şifreler eşleşmiyor', 'Close', { duration: 3000 });
@@ -46,6 +57,25 @@ export class AllBookShowComponent implements OnInit {
         }
       }
     )
+  }
+  borrowBook(bookName:string){
+    
+    this.bookNameDto={bookName :bookName}
+    console.log(this.bookNameDto.bookName);
+    
+    this.allBookShowService.addBorrowedBook(this.bookNameDto,this.userName).subscribe({
+      next:()=>{
+        this.snackBar.open("Kitap Basarili bir sekilde odunc alindi","Close",{duration:3000});
+        this.router.navigate(["all-books"]);
+
+      },
+      error:(error)=>{
+        this.snackBar.open("Kitap odunc alinamadi")
+        console.log(error);
+        
+      }
+    })
+    
   }
 
 
