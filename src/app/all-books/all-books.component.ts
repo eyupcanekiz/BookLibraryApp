a2f598acfe2f7dc7d7588a23db04f892207e6001
eyupcan0 <eyupcanekizz@gmail.com>
2024-08-16 17:47:45 +0300

    previous next button and book list

--- src/app/all-books/all-books.component.ts
+++ src/app/all-books/all-books.component.ts
@@ -12,26 +12,26 @@ export class AllBooksComponent implements OnInit {
   books: Book[] = [];
   errorMessage: string = '';
   searchTerm: string = '';
+  itemsPerPage: number = 15;
+  currentPage: number = 1;
+  paginatedBooks: Book[] = [];
+
   constructor(
      private bookService: BookService,
      private router: Router,
      private spinner: NgxSpinnerService  
     ) {}
   
-
   ngOnInit(): void {
     this.spinner.show();
     this.getAllBooks();
-    
   }
 
   getAllBooks() {
     this.bookService.getBooks().subscribe(
       (data: Book[]) => {
         this.books = data;
-       
-        
-   
+        this.filterAndPaginateBooks(); // Arama ve sayfalama işlemini birlikte yap
       },
       (error) => {
         this.errorMessage = error.message;
@@ -43,13 +43,38 @@ export class AllBooksComponent implements OnInit {
   }
 
   viewBookDetails(name: string) {
-    
-    
-    this.router.navigate(['/all-book-show',name]);
+    this.router.navigate(['/all-book-show', name]);
   }
-  filteredBooks() {
+  filterAndPaginateBooks() {
+    // Tüm kitapları filtrele
+    const filtered = this.books.filter(book => 
+      book.bookName.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+    
+    // Filtrelenmiş kitapları sayfalara böl
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedBooks = filtered.slice(startIndex, endIndex);
+  }
+
+  filteredBooks(): Book[] {
     return this.books.filter(book => 
       book.bookName.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
+  }
+
+  goToNextPage(){
+    const totalFilteredBooks = this.filteredBooks().length;
+    if (this.currentPage * this.itemsPerPage < totalFilteredBooks) {
+      this.currentPage++;
+      this.filterAndPaginateBooks();
+    }
+  }
+
+  goToPreviousPage(){
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.filterAndPaginateBooks();
+    }
   }
 }
